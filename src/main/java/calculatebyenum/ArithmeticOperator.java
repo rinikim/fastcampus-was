032955051d319ec2b1a0b9d8ebd7f555f889427e
rinikim <tytntu@naver.com>
2022-10-26 18:59:11 +0900

    계산기 로직 추가

---
+++ src/main/java/calculatebyenum/ArithmeticOperator.java
@@ -0,0 +1,46 @@
+package calculatebyenum;
+
+import java.util.Arrays;
+
+public enum ArithmeticOperator {
+    // 연산에 맞게끔 추상메소드를 구현할 수 있다.
+    ADDITION("+") {
+        @Override
+        public int arithmeticCalculate(int operand1, int operand2) {
+            return operand1 + operand2;
+        }
+    }, SUBTRACTION("-") {
+        @Override
+        public int arithmeticCalculate(int operand1, int operand2) {
+            return operand1 - operand2;
+        }
+    }, MULTIPLICATION("*") {
+        @Override
+        public int arithmeticCalculate(int operand1, int operand2) {
+            return operand1 * operand2;
+        }
+    }, DIVISION("/") {
+        @Override
+        public int arithmeticCalculate(int operand1, int operand2) {
+            return operand1 / operand2;
+        }
+    };
+
+    private final String operator;
+
+    ArithmeticOperator(String operator) {
+        this.operator = operator;
+    }
+
+    // 각각의 연산을 사용하게 해주고 싶음 -> 추상메소드 선언
+    public abstract int arithmeticCalculate(final int operand1, final int operand2);
+
+    public static int calculate(int operand1, String operator, int operand2) {
+        ArithmeticOperator arithmeticOperator = Arrays.stream(values())
+                .filter(v -> v.operator.equals(operator))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("올바른 사칙연산이 아닙니다."));
+
+        return arithmeticOperator.arithmeticCalculate(operand1, operand2);
+    }
+}
---
+++ src/main/java/calculatebyinterface/NewArithmeticOperator.java
@@ -0,0 +1,8 @@
+package calculatebyinterface;
+
+public interface NewArithmeticOperator {
+    boolean supports(String operator);
+
+    int calculate(PositiveNumber operand1, PositiveNumber operand2);
+
+}
